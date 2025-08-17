4aaf46dbefdaaaf59824bd33016e30f617f52c23
crxyne <craynemail@gmail.com>
2025-08-17 03:24:58 +0200

    more macro magic; prepare for registries

--- include/mc_cpp/common/macro_magic.hpp
+++ include/mc_cpp/common/macro_magic.hpp
@@ -8,17 +8,15 @@ namespace mc {
     static constexpr size_t fieldIndex = 0;
 
     template<size_t N, typename T, template <size_t, typename> typename U>
-    static constexpr size_t fieldIndex<N, T, U, std::void_t<decltype(U<N, T>::fieldName)> > =
-            fieldIndex<N + 1, T, U> + 1;
+    static constexpr size_t fieldIndex<N, T, U, std::void_t<decltype(U<N, T>::fieldName)>> = fieldIndex<N + 1, T, U> + 1;
 
     template<typename T, typename Tuple>
     constexpr bool tuple_contains_type = false;
 
     template<typename T, typename... Ts>
-    constexpr bool tuple_contains_type<T, std::tuple<Ts...> > =
-            std::disjunction_v<std::is_same<T, Ts>...>;
+    constexpr bool tuple_contains_type<T, std::tuple<Ts...>> = std::disjunction_v<std::is_same<T, Ts>...>;
 
-    template<typename T, typename Tuple, bool Found = tuple_contains_type<T, Tuple> >
+    template<typename T, typename Tuple, bool Found = tuple_contains_type<T, Tuple>>
     struct get_tuple_index_impl;
 
     template<typename T, typename Tuple>
@@ -33,7 +31,7 @@ namespace mc {
 
     template<typename T, typename U, typename... Ts>
     struct get_tuple_index_impl<T, std::tuple<U, Ts...>, true> {
-        static constexpr size_t value = 1 + get_tuple_index_impl<T, std::tuple<Ts...> >::value;
+        static constexpr size_t value = 1 + get_tuple_index_impl<T, std::tuple<Ts...>>::value;
     };
 
     template<typename T, typename Tuple>
@@ -55,11 +53,12 @@ namespace mc {
     using Name##_type = Type;                                                                                      \
     Name##_type Name = DefaultValue;                                                                               \
     template <size_t, typename> struct FieldData;                                                                  \
-    static constexpr size_t Name##_index = mc::fieldIndex<0, struct Dummy##Name, FieldData>;  \
+    static constexpr size_t Name##_index = mc::fieldIndex<0, struct Dummy##Name, FieldData>;                       \
                                                                                                                    \
     template <typename T>                                                                                          \
     struct FieldData<Name##_index, T> {                                                                            \
         static constexpr auto fieldName = #Name;                                                                   \
+        static constexpr auto fieldOptional = true;                                                                \
         using fieldType = Type;                                                                                    \
         static constexpr auto fieldCategory = Category;                                                            \
                                                                                                                    \
@@ -68,23 +67,46 @@ namespace mc {
     };                                                                                                             \
 static_assert(true, "Forcing ;")
 
-#define DECLARE_CONFIG_BACKEND                                                                                  \
+#define NAMED_FIELD_SET(Type, Name)                                                           \
+    using Name##_type = Type;                                                                 \
+    template <size_t, typename> struct FieldData;                                             \
+    static constexpr size_t Name##_index = mc::fieldIndex<0, struct Dummy##Name, FieldData>;  \
+                                                                                              \
+    template <typename T>                                                                     \
+    struct FieldData<Name##_index, T> {                                                       \
+        static constexpr auto fieldName = #Name;                                              \
+        static constexpr auto fieldOptional = Name##_optional;                                \
+        using fieldType = Type;                                                               \
+                                                                                              \
+        template <typename Parent> static constexpr fieldType                                 \
+        Parent::*fieldPtr = &Parent::Name;                                                    \
+    };                                                                                        \
+static_assert(true, "Forcing ;")
+
+#define NAMED_FIELD_OPTIONAL(Type, Name, DefaultValue)       \
+    Type Name = DefaultValue;                               \
+    static constexpr bool Name##_optional = true;           \
+    NAMED_FIELD_SET(Type, Name);                            \
+static_assert(true, "Forcing ;")
+
+#define NAMED_FIELD(Type, Name)                         \
+    Type Name{};                                        \
+    static constexpr bool Name##_optional = false;      \
+    NAMED_FIELD_SET(Type, Name);                        \
+static_assert(true, "Forcing ;")
+
+#define DECLARE_ENTRY_TO_JSON(Self)   auto to_json(nlohmann::ordered_json &json, const Self &self) -> void;
+#define DECLARE_ENTRY_FROM_JSON(Self) auto from_json(const nlohmann::json &json, Self &self) -> void;
+
+#define DECLARE_ENTRY_BACKEND                                                                                   \
     public:                                                                                                     \
-        [[nodiscard]]                                                                                           \
-        auto toJson() const -> nlohmann::ordered_json;                                                          \
-                                                                                                                \
-        auto loadFromJson(const nlohmann::json &json) -> void;                                                  \
-                                                                                                                \
-        auto save(const std::filesystem::path &filepath) const -> void;                                         \
-                                                                                                                \
-        [[nodiscard]]                                                                                           \
-        auto load(const std::filesystem::path &filepath) -> bool;                                               \
-                                                                                                                \
-    private:                                                                                                    \
-        static constexpr size_t numFields = mc::fieldIndex<0, void, FieldData>;                                \
+        static constexpr size_t numFields = mc::fieldIndex<0, void, FieldData>;                                 \
                                                                                                                 \
         template<size_t i>                                                                                      \
         static constexpr auto fieldName = FieldData<i, void>::fieldName;                                        \
+                                                                                                                \
+        template<size_t i>                                                                                      \
+        static constexpr auto fieldOptional = FieldData<i, void>::fieldOptional;                                \
                                                                                                                 \
         template<size_t i>                                                                                      \
         static constexpr auto fieldCategory = FieldData<i, void>::fieldCategory;                                \
@@ -103,35 +125,69 @@ static_assert(true, "Forcing ;")
         }                                                                                                       \
 static_assert(true, "Forcing ;")
 
-#define DEFINE_CONFIG_BACKEND(Self)                                                 \
-    auto Self::toJson() const -> nlohmann::ordered_json {                           \
-        nlohmann::ordered_json json;                                                \
-        mc::loop<numFields>([&](auto i) {                                          \
-           nlohmann::ordered_json j = this->getField<i>();                          \
-           json[fieldCategory<i>][fieldName<i>] = j;                                \
-        });                                                                         \
-        return json;                                                                \
-    }                                                                               \
-                                                                                    \
-    auto Self::loadFromJson(const nlohmann::json &json) -> void {                   \
-        mc::loop<numFields>([&](auto i) {                                          \
-            json.at(fieldCategory<i>).at(fieldName<i>).get_to(this->getField<i>()); \
-        });                                                                         \
-    }                                                                               \
-                                                                                    \
-    auto Self::save(const std::filesystem::path &filepath) const -> void {          \
-        std::ofstream file(filepath);                                               \
-        file << toJson().dump(4);                                                   \
-        file.close();                                                               \
-    }                                                                               \
-                                                                                    \
-    auto Self::load(const std::filesystem::path &filepath) -> bool {                \
-        std::ifstream file(filepath);                                               \
-        if (!file.is_open()) return false;                                          \
-        nlohmann::json json;                                                        \
-        file >> json;                                                               \
-        file.close();                                                               \
-        loadFromJson(json);                                                         \
-        return true;                                                                \
-    }                                                                               \
+#define DECLARE_WRITABLE_ENTRY_BACKEND                                                                          \
+    public:                                                                                                     \
+        auto save(const std::filesystem::path &filepath) const -> void;                                         \
+                                                                                                                \
+        [[nodiscard]]                                                                                           \
+        auto load(const std::filesystem::path &filepath) -> bool;                                               \
+                                                                                                                \
+    DECLARE_ENTRY_BACKEND;                                                                                      \
+static_assert(true, "Forcing ;")
+
+#define DEFINE_ENTRY_SAVE(Self)                                         \
+auto Self::save(const std::filesystem::path &filepath) const -> void {  \
+    std::ofstream file(filepath);                                       \
+    nlohmann::ordered_json json;                                        \
+    to_json(json, *this);                                               \
+    file << json.dump(4);                                               \
+    file.close();                                                       \
+}                                                                       \
+static_assert(true, "Forcing ;")
+
+#define DEFINE_ENTRY_LOAD(Self)                                     \
+auto Self::load(const std::filesystem::path &filepath) -> bool {    \
+    std::ifstream file(filepath);                                   \
+    if (!file.is_open()) return false;                              \
+    nlohmann::json json;                                            \
+    file >> json;                                                   \
+    file.close();                                                   \
+    from_json(json, *this);                                         \
+    return true;                                                    \
+}                                                                   \
+static_assert(true, "Forcing ;")
+
+#define DEFINE_CONFIG_BACKEND(Self)                                                             \
+    auto to_json(nlohmann::ordered_json &json, const Self &self) -> void {                      \
+        mc::loop<Self::numFields>([&](auto i) {                                                 \
+            nlohmann::ordered_json j = self.getField<i>();                                      \
+            json[Self::template fieldCategory<i>][Self::template fieldName<i>] = j;             \
+        });                                                                                     \
+    }                                                                                           \
+                                                                                                \
+    auto from_json(const nlohmann::json &json, Self &self) -> void {                            \
+        mc::loop<Self::numFields>([&](auto i) {                                                 \
+            json.at(Self::template fieldCategory<i>).at(Self::template fieldName<i>)            \
+                    .get_to(self.template getField<i>());                                       \
+        });                                                                                     \
+    }                                                                                           \
+static_assert(true, "Forcing ;")
+
+#define DEFINE_ENTRY_TO_JSON(Self)                                          \
+    auto to_json(nlohmann::ordered_json &json, const Self &self) -> void {  \
+        mc::loop<Self::numFields>([&](auto i) {                             \
+            nlohmann::ordered_json j = self.template getField<i>();         \
+            json[Self::template fieldName<i>] = j;                          \
+        });                                                                 \
+    }                                                                       \
+static_assert(true, "Forcing ;")
+
+#define DEFINE_ENTRY_FROM_JSON(Self)                                                            \
+    auto from_json(const nlohmann::json &json, Self &self) -> void {                            \
+        mc::loop<Self::numFields>([&](auto i) {                                                 \
+            if (!json.contains(Self::template fieldName<i>) && Self::template fieldOptional<i>) \
+                return;                                                                         \
+            json.at(Self::template fieldName<i>).get_to(self.template getField<i>());           \
+        });                                                                                     \
+    }                                                                                           \
 static_assert(true, "Forcing ;")
