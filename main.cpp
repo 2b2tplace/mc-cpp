3c671fa3ed597829005adc480713763e0a4910ea
crxyne <craynemail@gmail.com>
2026-03-19 18:26:59 +0100

    remove sqlite dependency

--- main.cpp
+++ main.cpp
@@ -4,18 +4,7 @@
 #include <mc_cpp/registry/minecraft.hpp>
 
 int main() {
-    mc::Logger logger{std::cout};
-    const auto res = mc::MinecraftRegistry::load("registries/765");
-    if (!res.has_value()) {
-        std::cerr << res.error() << '\n';
-        return EXIT_FAILURE;
-    }
-    const auto &registry = res.value();
-
-    const auto plainsBiomeType = registry.biomeType("plains");
-    const auto grassBlock = registry.blockType("grass_block").min;
-    const auto grassBlockColorPlains = registry.biomeBlockStateColor(plainsBiomeType, 69, grassBlock);
-
-    logger.log<mc::INFO>("grass block color in plains: {}", grassBlockColorPlains);
+    mc::Logger log{std::cout};
+    log.log<mc::INFO>("Hello, {}!", "world");
     return EXIT_SUCCESS;
 }
